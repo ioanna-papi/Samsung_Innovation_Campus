38a9642ddf73454a4b8c608aba5dc8a02987506b
Ioanna Papidaki <57221590+ioanna-papi@users.noreply.github.com>
2022-01-15 08:22:18 +0200

    Update PYTHON-TEAM3.py

--- Dataset/PYTHON-TEAM3.py
+++ Dataset/PYTHON-TEAM3.py
@@ -40,8 +40,8 @@ df = df.dropna(axis=0, how = 'any', subset=['Description', 'CustomerID'])
 
 # 2nd question
 for x in df.index :
-        if df.loc[x,'Description']=='AMAZON FEE' or df.loc[x,'Description']=='Manual' or df.loc[x,'Description']=='SAMPLES' or df.loc[x,'Description']=='POSTAGE' or df.loc[x,'Description']=='PACKING CHARGE':
-           df.drop(x, inplace = True)
+    if df.loc[x,'Description']=='AMAZON FEE' or df.loc[x,'Description']=='Manual' or df.loc[x,'Description']=='SAMPLES' or df.loc[x,'Description']=='POSTAGE' or df.loc[x,'Description']=='PACKING CHARGE':
+        df.drop(x, inplace = True)
         
 # 3rd question
 
